3e6ac4a0c3644721d22262605ef67efb32c716cb
kimdonghun <zhfptm12@gmail.com>
2023-02-15 12:52:45 +0900

    django web socket

---
+++ new/chat/consumers.py
@@ -0,0 +1,78 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from django.core.cache import cache
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        print("self.scope.session : {}".format(self.scope['session']))
+        print("self.user : {}".format(self))
+        print("self.scope.user : {}".format(self.scope['user'].pk))
+        print("AsyncWebSocketConsumer : {}".format(AsyncWebsocketConsumer))
+        print("self.scope : {}".format(self.scope))
+    	# 파라미터 값으로 채팅 룸을 구별
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # 룸 그룹에 참가
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+
+    async def disconnect(self, close_code):
+        # 룸 그룹 나가기
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # 웹소켓으로부터 메세지 받음
+
+    async def receive(self, text_data):
+        a = ['1','2']
+        a.append('3')
+        print(a)
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        print(text_data)
+        print("web socket")
+        l = list(text_data_json.items())
+        print(l)
+        if cache.get('chat_data'):
+            print("exist")
+            caches = cache.get('chat_data')
+            print(caches)
+            print(type(a))
+            print(type(caches))
+            caches.append(l)
+            print(caches)
+            cache.set('chat_data', caches)
+
+        else:
+            cache.set('chat_data',l)
+        datas = cache.get('chat_data')
+        print(type(text_data_json))
+        # 룸 그룹으로 메세지 보냄
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': datas,
+                'id' : text_data_json['user_id']
+            }
+        )
+
+    # 룸 그룹으로부터 메세지 받음
+    async def chat_message(self, event):
+        message = event['message']
+        print("room")
+        # 웹소켓으로 메세지 보냄
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'id': event['id']
+
+        }))
